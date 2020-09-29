44e4112795ecc6f9874f90052be4f84d353505af
moliayesha7 <moliayesha7@gmail.com>
2020-09-29 15:54:54 -0700

    cc

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const url = process.env.REACT_APP_API_URL;
   return (
     <div className="App">
-     {process.env.REACT_APP_API_URL}
+     {url}
     </div>
   );
 }
